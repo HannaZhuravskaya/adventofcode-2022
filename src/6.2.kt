b513b08cd2ce744510a655313fe2f12a335d3279
Hanna Zhuravskaya <hanna.zhuravskaya@gmail.com>
2022-12-09 17:56:14 +0100

    Add solution for day 6

---
+++ src/6.2.kt
@@ -0,0 +1,39 @@
+package adventofcode2022
+
+import java.io.File
+import java.util.ArrayDeque
+
+fun readFileAsLine(fileName: String): String = File(fileName).readLines()[0]
+
+fun main(args:Array<String>) {
+    var line = readFileAsLine("/Users/hzhuravskaya/Documents/adventofcode-2022/inputs/6.2.txt")
+
+    val usedChar:MutableMap<Char, Int> = mutableMapOf();  
+    var cnt = 0;
+
+    for((i, ch) in line.withIndex())
+    {
+        if(usedChar.get(ch) == null){
+            cnt++
+        } 
+        else
+        {
+            var prevCharIndex = usedChar.getValue(ch)
+
+            if(i - prevCharIndex <= cnt){
+                cnt = i - prevCharIndex
+            }
+            else{
+                cnt++
+            }
+        }
+
+        usedChar.put(ch, i)
+
+        if(cnt == 14)
+        {
+            println(i+1)
+            break
+        }
+    }    
+}
