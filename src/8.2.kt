7addf007b38bcc0c2260abba2a9b0e7b0ccdf667
Hanna Zhuravskaya <hanna.zhuravskaya@gmail.com>
2022-12-10 19:19:47 +0100

    Add solution for day 8

---
+++ src/8.2.kt
@@ -0,0 +1,94 @@
+package adventofcode2022
+
+import java.io.File
+import kotlin.math.*
+
+fun readFileAsLines(fileName: String): List<String> = File(fileName).readLines()
+
+fun max(a: Int, b: Int): Int{
+    return if(a > b) a else b
+}
+
+fun main(args:Array<String>) {
+    var lines = readFileAsLines("/Users/hzhuravskaya/Documents/adventofcode-2022/inputs/8.2.txt")
+
+    var n = lines.size
+    var m = lines[0].length
+    val arr = Array(n) { IntArray(m) }
+    
+    for(i in 0..n-1){
+        for(j in 0..m-1){
+            var curTree = lines[i][j].digitToInt()
+            var value = 1
+            
+            // left
+            var cnt = 0
+            var k = j - 1
+            while(k > -1){
+                ++cnt
+                
+                if(lines[i][k].digitToInt() >= curTree){
+                    break
+                }
+
+                --k
+            }
+            value *= cnt
+
+            // right
+            cnt = 0
+            k = j + 1
+            while(k < m){
+                ++cnt
+
+                if(lines[i][k].digitToInt() >= curTree){
+                    break
+                }
+
+                ++k
+            }
+            value *= cnt
+
+            // top
+            cnt = 0
+            k = i - 1
+            while(k > -1){
+                ++cnt
+
+                if(lines[k][j].digitToInt() >= curTree){
+                    break
+                }
+
+                --k
+            }
+            value *= cnt
+
+            // bottom
+            cnt = 0
+            k = i + 1
+            while(k < n){
+                ++cnt
+
+                if(lines[k][j].digitToInt() >= curTree){
+                    break
+                }
+
+                ++k
+            }
+            value *= cnt
+
+            arr[i][j] = value
+        }
+    }
+
+    var max = 0
+    for(i in 0..n-1){
+        for(j in 0..m-1){
+            if(arr[i][j] > max){
+                max = arr[i][j]
+            }
+        }
+    }
+
+    println (max)
+}
---
+++ src/8.kt
@@ -0,0 +1,85 @@
+package adventofcode2022
+
+import java.io.File
+import kotlin.math.*
+
+fun readFileAsLines(fileName: String): List<String> = File(fileName).readLines()
+
+fun max(a: Int, b: Int): Int{
+    return if(a > b) a else b
+}
+
+fun main(args:Array<String>) {
+    var lines = readFileAsLines("/Users/hzhuravskaya/Documents/adventofcode-2022/inputs/8.txt")
+
+    var n = lines.size
+    var m = lines[0].length
+    val arr = Array(n) { IntArray(m) }
+    
+    //left
+    for(i in 0..n-1){
+        var max = 0
+        for(j in 0..m-1){
+            if(j > 0){
+                max = max(max, lines[i][j-1].digitToInt())
+            }
+
+            if(lines[i][j].digitToInt() > max || j == 0){
+                arr[i][j] = 1
+            }
+        }
+    }
+
+    //right
+    for(i in n-1 downTo 0){
+        var max = 0
+        for(j in m-1 downTo 0){
+            if(j < m-1){
+                max = max(max, lines[i][j+1].digitToInt())
+            }
+
+            if(lines[i][j].digitToInt() > max || j == m-1){
+                arr[i][j] = 1
+            }
+        }
+    }
+
+    //top
+    for(j in 0..m-1){
+        var max = 0
+        for(i in 0..n-1){
+            if(i > 0){
+                max = max(max, lines[i-1][j].digitToInt())
+            }
+
+            if(lines[i][j].digitToInt() > max || i == 0){
+                arr[i][j] = 1
+            }
+        }
+    }
+
+    //bottom
+    for(j in m-1 downTo 0){
+        var max = 0
+        for(i in n-1 downTo 0){
+            if(i < n-1){
+                max = max(max, lines[i+1][j].digitToInt())
+            }
+
+            if(lines[i][j].digitToInt() > max || i == n-1){
+                arr[i][j] = 1
+            }
+        }
+    }
+
+    var cnt = 0
+    for(i in 0..n-1){
+        for(j in 0..m-1){
+            if(arr[i][j] == 1){
+                cnt++
+            }
+        }
+    }
+
+    println (cnt)
+}
